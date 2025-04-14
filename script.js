e8ecd73138d98e7bf6707badd8511d8f64e9369b
GabrielSalomao0104 <gsalomaob27@gmail.com>
2025-04-14 08:28:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+var paises = ['Argentina', 'Canada', 'Uruguai', 'Azerbaijão', 'Ilhas Salomão', 'Brasil','Estados Unidos', 'Rússia', 'Peru','Angola','Equador']
+var paisEscolhido = parseFloat(prompt("Digite um país de 0 a 10"))
+document.write("País escolhido: " + paises[paisEscolhido])
